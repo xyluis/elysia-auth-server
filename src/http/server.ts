1141588ee85f123d654da4abd63bb57224ab861e
Luís Felipe <luisfelipe.assuncao@hotmail.com>
2024-01-06 23:09:26 -0400

    fix: update required origin

--- src/http/server.ts
+++ src/http/server.ts
@@ -17,7 +17,7 @@ const app = new Elysia()
       origin: (request): boolean => {
         const origin = request.headers.get('origin')
 
-        if (!origin || origin !== 'http://localhost:3000') {
+        if (!origin) {
           return false
         }
 
